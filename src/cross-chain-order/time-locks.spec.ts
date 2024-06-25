33e0e6a9b494ff77715379c820131ca208f88f85
Vladimir Borovik <v.borovik@1inch.io>
2024-06-25 21:09:51 +0300

    TimeLock class update

--- src/cross-chain-order/time-locks.spec.ts
+++ src/cross-chain-order/time-locks.spec.ts
@@ -1,25 +1,31 @@
-import {randBigInt} from '@1inch/fusion-sdk'
-import {UINT_256_MAX, UINT_32_MAX} from '@1inch/byte-utils'
+import {UINT_32_MAX} from '@1inch/byte-utils'
 import {TimeLocks} from './time-locks'
 
 describe('TimeLocks', () => {
     it('Should encode/decode', () => {
-        const timeLock = TimeLocks.fromBigInt(randBigInt(UINT_256_MAX))
+        const timeLock = TimeLocks.new({
+            srcWithdrawal: 1n,
+            srcPublicWithdrawal: 2n,
+            srcCancellation: 3n,
+            srcPublicCancellation: 4n,
+            dstWithdrawal: 1n,
+            dstPublicWithdrawal: 2n,
+            dstCancellation: 3n
+        })
 
         expect(TimeLocks.fromBigInt(timeLock.build())).toEqual(timeLock)
     })
 
     it('Should encode/decode max value', () => {
-        const timeLock = TimeLocks.fromBigInt(UINT_256_MAX)
-
+        const timeLock = TimeLocks.new({
+            srcWithdrawal: UINT_32_MAX - 3n,
+            srcPublicWithdrawal: UINT_32_MAX - 2n,
+            srcCancellation: UINT_32_MAX - 1n,
+            srcPublicCancellation: UINT_32_MAX,
+            dstWithdrawal: UINT_32_MAX - 2n,
+            dstPublicWithdrawal: UINT_32_MAX - 1n,
+            dstCancellation: UINT_32_MAX
+        })
         expect(TimeLocks.fromBigInt(timeLock.build())).toEqual(timeLock)
-        expect(timeLock.deployedAt).toEqual(UINT_32_MAX)
-        expect(timeLock.srcWithdrawal).toEqual(UINT_32_MAX * 2n)
-        expect(timeLock.srcPublicWithdrawal).toEqual(UINT_32_MAX * 2n)
-        expect(timeLock.srcCancellation).toEqual(UINT_32_MAX * 2n)
-        expect(timeLock.srcPublicCancellation).toEqual(UINT_32_MAX * 2n)
-        expect(timeLock.dstWithdrawal).toEqual(UINT_32_MAX * 2n)
-        expect(timeLock.dstPublicWithdrawal).toEqual(UINT_32_MAX * 2n)
-        expect(timeLock.dstCancellation).toEqual(UINT_32_MAX * 2n)
     })
 })
--- src/cross-chain-order/time-locks.ts
+++ src/cross-chain-order/time-locks.ts
@@ -1,7 +1,17 @@
 import {BitMask, BN, UINT_32_MAX} from '@1inch/byte-utils'
 import assert from 'assert'
 
-// todo check intervals and add docs
+/**
+ * Contains the duration of each stage of swap for source and destination chain
+ *
+ * Source chain intervals layout
+ * | finality lock | resolver withdraw | public withdraw | resolver cancellation | public cancellation |
+ * ^deployedAt
+ *
+ * Destination chain intervals layout
+ * | finality lock | resolver withdraw | public withdraw | resolver cancellation |
+ * ^deployedAt
+ */
 export class TimeLocks {
     static Web3Type = 'uint256'
 
@@ -25,25 +35,51 @@ export class TimeLocks {
             'srcWithdrawal can not be > uint32 max value'
         )
         assert(
+            _srcWithdrawal < _srcPublicWithdrawal,
+            'srcWithdrawal can not be >= srcPublicWithdrawal'
+        )
+
+        assert(
             _srcPublicWithdrawal <= UINT_32_MAX,
             'srcPublicWithdrawal can not be > uint32 max value'
         )
+        assert(
+            _srcPublicWithdrawal < _srcCancellation,
+            'srcPublicWithdrawal can not be >= srcCancellation'
+        )
+
         assert(
             _srcCancellation <= UINT_32_MAX,
             'srcCancellation can not be > uint32 max value'
         )
         assert(
+            _srcCancellation < _srcPublicCancellation,
+            'srcCancellation can not be >= srcPublicCancellation'
+        )
+
+        assert(
             _srcPublicCancellation <= UINT_32_MAX,
             'srcPublicCancellation can not be > uint32 max value'
         )
+
         assert(
             _dstWithdrawal <= UINT_32_MAX,
             'dstWithdrawal can not be > uint32 max value'
         )
         assert(
+            _dstWithdrawal < _dstPublicWithdrawal,
+            'dstWithdrawal can not be >= dstPublicWithdrawal'
+        )
+
+        assert(
             _dstPublicWithdrawal <= UINT_32_MAX,
             'dstPublicWithdrawal can not be > uint32 max value'
         )
+        assert(
+            _dstPublicWithdrawal < _dstCancellation,
+            'dstPublicWithdrawal can not be >= dstCancellation'
+        )
+
         assert(
             _dstCancellation <= UINT_32_MAX,
             'dstCancellation can not be > uint32 max value'
@@ -79,17 +115,44 @@ export class TimeLocks {
     }
 
     public static new(params: {
-        deployedAt: bigint
+        /**
+         * Network: Source, Destination
+         * By default, is 0n
+         *
+         * Set onchain when escrow deployed
+         */
+        deployedAt?: bigint
+        /**
+         * Network: Source
+         * Delay from `deployedAt` at which ends `finality lock` and starts `private withdrawal` */
         srcWithdrawal: bigint
+        /**
+         * Network: Source
+         * Delay from `deployedAt` at which ends `private withdrawal` and starts `public withdrawal` */
         srcPublicWithdrawal: bigint
+        /**
+         * Network: Source
+         * Delay from `deployedAt` at which ends `public withdrawal` and starts `private cancellation` */
         srcCancellation: bigint
+        /**
+         * Network: Source
+         * Delay from `deployedAt` at which ends `private cancellation` and starts `public cancellation` */
         srcPublicCancellation: bigint
+        /**
+         * Network: Destination
+         * Delay from `deployedAt` at which ends `finality lock` and starts `private withdrawal` */
         dstWithdrawal: bigint
+        /**
+         * Network: Destination
+         * Delay from `deployedAt` at which ends `private withdrawal` and starts `public withdrawal` */
         dstPublicWithdrawal: bigint
+        /**
+         * Network: Destination
+         * Delay from `deployedAt` at which ends `public withdrawal` and starts `private cancellation` */
         dstCancellation: bigint
     }): TimeLocks {
         return new TimeLocks(
-            params.deployedAt,
+            params.deployedAt || 0n,
             params.srcWithdrawal,
             params.srcPublicWithdrawal,
             params.srcCancellation,
