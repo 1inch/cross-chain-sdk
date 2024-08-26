7da7a3d6b5f57e4def4e1d162c0833f090b1d8f6
Alexandr <a.volkov@1inch.io>
2024-08-26 11:37:22 +0200

    chore: wip on types

--- src/ws-api/constants.ts
+++ src/ws-api/constants.ts
@@ -1,11 +1,11 @@
-import {OrderEventType} from './types'
+import {EventType, OrderEventType} from './types'
 
 export const orderEvents: OrderEventType['event'][] = [
-    'order_created',
-    'order_invalid',
-    'order_balance_or_allowance_change',
-    'order_filled',
-    'order_filled_partially',
-    'order_cancelled',
-    'secret_shared'
+    EventType.OrderCreated,
+    EventType.OrderInvalid,
+    EventType.OrderBalanceOrAllowanceChange,
+    EventType.OrderFilled,
+    EventType.OrderFilledPartially,
+    EventType.OrderCancelled,
+    EventType.OrderSecretShared
 ]
