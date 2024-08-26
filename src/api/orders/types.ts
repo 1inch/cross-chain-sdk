0607f62aba50e3f43a6ad60427a9311982d3db24
Alexandr <a.volkov@1inch.io>
2024-08-26 15:30:07 +0200

    chore: secret hashes

--- src/api/orders/types.ts
+++ src/api/orders/types.ts
@@ -27,7 +27,7 @@ export type ActiveOrder = {
     srcChainId: SupportedChain
     dstChainId: SupportedChain
     isMakerContract: boolean
-    secrets: PublicSecret[]
+    secretHashes: string[]
 }
 
 export type ActiveOrdersResponse = PaginationOutput<ActiveOrder>
