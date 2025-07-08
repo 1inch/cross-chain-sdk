e7ff934e4ee77c4b639f815df97a2b8c521d96f1
Vladimir Borovik <v.borovik@1inch.io>
2025-07-08 14:44:19 +0400

    fix: export EvmEscrowParams

--- src/cross-chain-order/index.ts
+++ src/cross-chain-order/index.ts
@@ -1,5 +1,11 @@
 export {EvmCrossChainOrder} from './evm-cross-chain-order'
 export {EscrowExtension} from './escrow-extension'
 export {HashLock} from './hash-lock'
-export {EscrowParams, Details, Extra, CrossChainOrderInfo} from './types'
+export {
+    EscrowParams,
+    EvmEscrowParams,
+    Details,
+    Extra,
+    CrossChainOrderInfo
+} from './types'
 export * from './time-locks'
