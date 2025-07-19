af8cdc93f30ebcba338c12f1a481403b0a35c806
Vladimir Borovik <v.borovik@1inch.io>
2025-07-19 16:05:06 +0400

    expors

--- src/cross-chain-order/svm/index.ts
+++ src/cross-chain-order/svm/index.ts
@@ -1,3 +1,3 @@
 export {ResolverCancellationConfig} from './resolver-cancellation-config'
-export {SvmCrossChainOrder} from './svm-cross-chain-order'
+export {SvmCrossChainOrder, SolanaOrderJSON} from './svm-cross-chain-order'
 export {SolanaDetails, SolanaEscrowParams, SolanaExtra} from './types'
