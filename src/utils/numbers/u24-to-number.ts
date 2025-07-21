bfab2bf31ac17a3e316ba9769c380a3f5348d3e9
Kirill Kuznetcov <krboktv@gmail.com>
2025-07-21 19:31:17 +0400

    chore: refactor

--- src/utils/numbers/u24-to-number.ts
+++ src/utils/numbers/u24-to-number.ts
@@ -1,5 +1,7 @@
 import {FixedLengthArray} from '../../type-utils'
 
 export function u24ToNumber(val: [FixedLengthArray<number, 3>]): number {
-    return parseInt('0x' + Buffer.from(val[0]).toString('hex'))
+    const bytes = val[0]
+
+    return (bytes[0] << 16) | (bytes[1] << 8) | bytes[2]
 }
