0f68f7477001555bf1187855a5f1993f5fc1c612
Vladimir Borovik <v.borovik@1inch.io>
2025-07-21 17:23:26 +0400

    test

---
+++ src/domains/addresses/evm-address.spec.ts
@@ -0,0 +1,11 @@
+import {EvmAddress} from './evm-address'
+
+describe('EvmAddress', () => {
+    it('Should create from buffer', () => {
+        const address = EvmAddress.fromBigInt(0xdeadbeefn)
+
+        expect(EvmAddress.fromBuffer(address.toBuffer()).toString()).toBe(
+            '0x00000000000000000000000000000000deadbeef'
+        )
+    })
+})
