ca17d2b15988397e14f8d988e797319a16cfe44b
Vladimir Borovik <v.borovik@1inch.io>
2025-07-24 17:48:48 +0400

    chore: exports

---
+++ src/idl/index.ts
@@ -0,0 +1,3 @@
+export {IDL as SOLANA_DST_ESCROW} from './cross-chain-escrow-dst'
+export {IDL as SOLANA_SRC_ESCROW} from './cross-chain-escrow-src'
+export {IDL as SOLANA_WHITELIST} from './whitelist'
