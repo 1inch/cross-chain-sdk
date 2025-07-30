e26e47d482d6e2ac759205ac13a973575ecefda5
Vladimir Borovik <v.borovik@1inch.io>
2025-07-30 12:05:29 +0300

    comment

--- src/domains/addresses/types.ts
+++ src/domains/addresses/types.ts
@@ -13,9 +13,10 @@ export type AddressLike = {
     isNative(): boolean
     isZero(): boolean
     toBigint(): bigint
-    /** Split the address into 2 parts
-     * Second parts is 40 bytes as valid evm address
-     * First bytes
+    /**
+     * Split the address into 2 parts
+     * Second parts is 20 bytes as valid evm address
+     * First bytes is rest bytes with length equal to `TotalBytes - 20`
      */
     splitToParts(): [AddressComplement, EvmAddress]
 }
